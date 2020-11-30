055dd078d77c1ee64ea1fe4766bca94824391370
think-jin <70687311+think-jin@users.noreply.github.com>
2020-11-30 10:40:40 +0900

    Delete datetime.py

--- final/datetime.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta
-
-now = datetime.now()
-
-alarmtime = datetime(now.year, now.month, now.day, 19, 31, 40)
-#now.day를 받을때, now.hour보다 alarmtime의 hour이 더 작으면 day+1
-print((alarmtime - now).seconds)
